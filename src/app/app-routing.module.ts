48eb54cb7ea1d4c3ee0b2f061c63a7fee5e12b83
Denys1994q <drybachok@gmail.com>
2023-07-30 11:19:20 +0300

    create structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewsPageComponent } from './news/pages/news-page/news-page.component';
 
-const routes: Routes = [];
+// сторінку помилки 
+const routes: Routes = [
+  {
+    path: '',
+    component: NewsPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/news/pages/news-page/news-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-component-name', 
+    templateUrl: './news-page.component.html', 
+    styleUrls: ['./news-page.component.sass'], 
+})
+
+export class NewsPageComponent implements OnInit {
+    // Змінні, властивості, методи і логіка компонента
+    // ...
+  
+    constructor() {}
+  
+    ngOnInit(): void {
+      // Логіка, що виконується при ініціалізації компонента
+    }
+  
+    // Інші методи компонента, якщо необхідно
+    // ...
+  }
